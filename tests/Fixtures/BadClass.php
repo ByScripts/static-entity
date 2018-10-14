63097739f6172b9ff534021aea50dc9b7c01f39c
Thierry Goettelmann <thierry@byscripts.info>
2018-10-14 23:38:52 +0200

    Clean code

--- tests/Fixtures/BadClass.php
+++ tests/Fixtures/BadClass.php
@@ -4,18 +4,4 @@ namespace Byscripts\StaticEntity\Tests\Fixtures;
 
 class BadClass
 {
-    private $foo;
-
-    static function getDataSet(): array
-    {
-        return [
-            1 => [
-                'foo' => 'bar',
-            ],
-            2 => [
-                'foo' => 'bar',
-                'bar' => 'baz',
-            ],
-        ];
-    }
 }
--- tests/Fixtures/BadProperty.php
+++ tests/Fixtures/BadProperty.php
@@ -6,17 +6,11 @@ use Byscripts\StaticEntity\AbstractStaticEntity;
 
 class BadProperty extends AbstractStaticEntity
 {
-    private $foo;
-
     static function getDataSet(): array
     {
         return [
             1 => [
                 'foo' => 'bar',
-            ],
-            2 => [
-                'foo' => 'bar',
-                'bar' => 'baz',
             ],
         ];
     }
